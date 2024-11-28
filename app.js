099f1e9387292b73167d964c5d3b6e60d507ce8a
Lerian Febriana <kanglerian@gmail.com>
2024-11-28 11:22:22 +0700

    use cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -9,6 +10,7 @@ const programsRouter = require('./routes/programs');
 
 const app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
