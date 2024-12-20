20c05df33f81680ac85aee1b2209c10801f175e0
Lerian Febriana <kanglerian@gmail.com>
2024-12-20 09:33:41 +0700

    origin

--- app.js
+++ app.js
@@ -10,7 +10,33 @@ const programsRouter = require('./routes/programs');
 
 const app = express();
 
-app.use(cors());
+const allowedOrigins = [
+  'https://test-gaya-belajar.politekniklp3i-tasikmalaya.ac.id',
+  'https://test-otak.politekniklp3i-tasikmalaya.ac.id',
+  'https://psikotest.politekniklp3i-tasikmalaya.ac.id',
+  'https://databases.politekniklp3i-tasikmalaya.ac.id',
+  'https://test-otak.politekniklp3i-tasikmalaya.ac.id',
+  'https://beasiswa.politekniklp3i-tasikmalaya.ac.id',
+  'https://sbpmb.politekniklp3i-tasikmalaya.ac.id',
+  'https://sie.politekniklp3i-tasikmalaya.ac.id',
+  'https://pmb.politekniklp3i-tasikmalaya.ac.id',
+  'https://politekniklp3i-tasikmalaya.ac.id',
+  'http://localhost:5173'
+];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
